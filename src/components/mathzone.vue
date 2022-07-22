5a11129a336bd3f402b47e05c883bfc8d2a96064
Gaëtan <gaetan.plisson@gmail.com>
2022-07-22 13:16:08 +0200

    mathzone

---
+++ src/components/mathzone.vue
@@ -0,0 +1,15 @@
+<template>
+  <span>Trucs en latex: {{latex}}</span>
+</template>
+
+<script>
+export default {
+  props: ["is-selected", "latex"]
+}
+</script>
+
+<style lang="css" scoped>
+textarea {
+  height: inherit
+}
+</style>
