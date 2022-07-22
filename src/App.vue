c8351a8958d8a3644f8ff3afc894bb8f352368cc
Gaëtan <gaetan.plisson@gmail.com>
2022-07-22 12:48:38 +0200

    graphic titles zones

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import headtitle from './components/headtitle.vue'
+import intitle from './components/intitle.vue'
+
 </script>
 
 <template>
   <div id="wrapper">
     <headtitle title="Mon devoir de math" name="tatan"  date="ven 22 juillet"/>
+    <intitle title="Problème de Math" power="1"></intitle>
+    
   </div>
 </template>
 
@@ -14,6 +18,7 @@ import headtitle from './components/headtitle.vue'
   #wrapper {
     background-color: var(--bs-gray-200);
     max-width: 80em;
-    margin: auto
+    margin: auto;
+    padding: 4em
   }
 </style>
---
+++ src/components/intitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 v-if="1==power">{{title}}</h1>
+  <h2 v-if="2==power">{{title}}</h2>
+  <h3 v-if="3==power">{{title}}</h3>
+  <h4 v-if="4==power">{{title}}</h4>
+  <h5 v-if="5==power">{{title}}</h5>
+  <h6 v-if="6==power">{{title}}</h6>
+
+</template>
+
+<script>
+export default {
+  props: ["title", "power"]
+}
+</script>
+
+<style lang="css" scoped>
+  h1 {
+    text-align: left
+  }
+</style>
